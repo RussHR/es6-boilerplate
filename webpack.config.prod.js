db343d699969f1c2a9af0ae0c27097639528e8de
RussHR <russ.rinzler@gmail.com>
2016-08-21 21:12:14 -0700

    set NODE_ENV to production when running a build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,6 +11,11 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 warnings: false,
